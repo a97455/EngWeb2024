94ea6bbcfc6e1636df9c8b7a61dea68d704e8ad2
a97455 <a97455@alunos.uminho.pt>
2024-02-29 16:18:07 +0000

    atores

--- TP3/servidor.js
+++ TP3/servidor.js
@@ -65,21 +65,14 @@ function createFilmeHTML(filme){
                 <h1>${filme.title}</h1>
             </header>
             
+            
             <div class="w3-container">
-                <table class="w3-table w3-striped">
-                    <tr>
-                        <th>Título</th>
-                        <th>Ano</th>
-                        <th>Elenco</th>
-                        <th>Género</th>
-                    </tr>
-                    <tr>
-                        <td>${filme.title}</td>
-                        <td>${filme.year}</td>
-                        <td>${filme.cast}</td>         
-                        <td>${filme.genres}</td>
-                    </tr>
-                </table>
+                <ul class="w3-ul w3-border">
+                    <li><strong>Título:</strong> ${filme.title}</li>
+                    <li><strong>Ano:</strong> ${filme.year}</li>
+                    <li><strong>Elenco:</strong> ${filme.cast}</li>
+                    <li><strong>Géneros:</strong> ${filme.genres}</li>
+                </ul>
             </div>
             
             <footer class="w3-container w3-blue">
@@ -150,14 +143,82 @@ function createGeneroHTML(genero){
             </header>
             
             <div class="w3-container">
+                <ul class="w3-ul w3-border">
+                    <li><strong>Nome:</strong> ${genero.name}</li>
+                </ul>
+            </div>
+            
+            <footer class="w3-container w3-blue">
+                <h5>TPC3 in EngWeb2024</h5>
+            </footer>
+        </div>
+    </body>
+</html>`
+
+    return pagHTML
+}
+
+
+function createListaAtoresHTML(generos){
+    pagHTML = `
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Lista de Atores</title>
+        <link rel="stylesheet" href="/w3.css"/>
+    </head>
+    <body>
+        <div class="w3-card-4">
+            <header class="w3-container w3-blue">
+              <h1>Lista de Atores</h1>
+            </header>
+            
+            <div class="w3-container">
                 <table class="w3-table w3-striped">
                     <tr>
                         <th>Nome</th>
-                    </tr>
+                    </tr>`
+    generos.forEach(function(ator){
+        pagHTML += `
                     <tr>
-                        <td>${genero.name}</td>
-                    </tr>
+                        <td><a href='/atores/${ator.id}'>${ator.name}</a></td>
+                    </tr>`
+    });
+    pagHTML += `
                 </table>
+            </div>
+            
+            <footer class="w3-container w3-blue">
+              <h5>TPC3 in EngWeb2024</h5>
+            </footer>
+        </div>
+    </body>
+</html>`
+    
+    return pagHTML
+}
+
+
+function createAtorHTML(ator){
+    pagHTML=`
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>${ator.name}</title>
+        <link rel="stylesheet" href="/w3.css"/>
+    </head>
+    <body>
+        <div class="w3-card-4">
+            <header class="w3-container w3-blue">
+                <h1>${ator.name}</h1>
+            </header>
+            
+            <div class="w3-container">
+                <ul class="w3-ul w3-border">
+                    <li><strong>Nome:</strong> ${ator.name}</li>
+                </ul>
             </div>
             
             <footer class="w3-container w3-blue">
@@ -174,6 +235,7 @@ function createGeneroHTML(genero){
 http.createServer(function (req, res) {
     var regexFilmes = /^\/filmes\/[a-z0-9]{24}$/
     var regexGeneros = /^\/generos\/\d+$/
+    var regexAtores = /^\/atores\/\d+$/
 
     var q = url.parse( req.url, true)
     if(q.pathname == '/'){
@@ -233,6 +295,36 @@ http.createServer(function (req, res) {
             .then(function(resp){
                 dados = resp.data
                 pagHTML = createGeneroHTML(dados)
+                res.writeHead(200, {'Content-Type': 'text/html'})
+                res.write(pagHTML)
+                res.end()
+            })
+            .catch(function(erro){
+                res.writeHead(500, {'Content-Type': 'text/html'})
+                res.write("<pre>" + erro + "</pre>")
+                res.end()
+            })
+    }
+    else if(q.pathname == '/atores'){
+        axios.get('http://localhost:3000/atores')
+            .then(function(resp){
+                dados = resp.data
+                pagHTML = createListaAtoresHTML(dados)
+                res.writeHead(200, {'Content-Type': 'text/html'})
+                res.write(pagHTML)
+                res.end()
+            })
+            .catch(function(erro){
+                res.writeHead(500, {'Content-Type': 'text/html'})
+                res.write("<pre>" + erro + "</pre>")
+                res.end()
+            })
+    }
+    else if(regexAtores.test(q.pathname)){
+        axios.get('http://localhost:3000'+q.pathname)
+            .then(function(resp){
+                dados = resp.data
+                pagHTML = createAtorHTML(dados)
                 res.writeHead(200, {'Content-Type': 'text/html'})
                 res.write(pagHTML)
                 res.end()
