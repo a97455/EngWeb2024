2dd8098911a9a9980cd7f28f069336c699c54bd9
a97455 <a97455@alunos.uminho.pt>
2024-03-07 10:24:27 +0000

    tp4 dataset

---
+++ TP4/jsonRemake.py
@@ -0,0 +1,30 @@
+import json
+import sys
+
+def processar_json(input_file):
+    with open(input_file, 'r') as f:
+        data = json.load(f)
+    
+    periodosDict = {}
+    compositoresDict = data['compositores']
+    
+    for compositor in compositoresDict:
+        if compositor['periodo'] not in periodosDict:
+            periodosDict[compositor['periodo']] = {'id': len(periodosDict)+1, 'name': compositor['periodo']}
+    
+    data['periodos'] = list(periodosDict.values())
+
+    for compositor in compositoresDict:
+        compositor['periodo'] = periodosDict[compositor['periodo']]
+
+    return data
+
+
+def salvar_json(data, output_file):
+    with open(output_file, 'w') as f:
+        json.dump(data, f, indent=2)
+
+
+if __name__ == "__main__":    
+    novo_json = processar_json(sys.argv[1])
+    salvar_json(novo_json, sys.argv[2])
