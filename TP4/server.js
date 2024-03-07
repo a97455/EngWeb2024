e1f90d829f7e5a85b4e6e6d577e2c0ca2c9484ac
a97455 <a97455@alunos.uminho.pt>
2024-03-07 10:46:48 +0000

    tp4 inicio

---
+++ TP4/server.js
@@ -0,0 +1,160 @@
+// alunos_server.js
+// EW2024 : 04/03/2024
+// by jcr
+
+var http = require('http')
+var axios = require('axios')
+const { parse } = require('querystring');
+
+var templates = require('./templates.js')        // Necessario criar e colocar na mesma pasta
+var static = require('./static.js')             // Colocar na mesma pasta
+
+// Aux functions
+function collectRequestBodyData(request, callback) {
+    if(request.headers['content-type'] === 'application/x-www-form-urlencoded') {
+        let body = '';
+        request.on('data', chunk => {
+            body += chunk.toString();
+        });
+        request.on('end', () => {
+            callback(parse(body));
+        });
+    }
+    else {
+        callback(null);
+    }
+}
+
+// Server creation
+
+var alunosServer = http.createServer((req, res) => {
+    // Logger: what was requested and when it was requested
+    var d = new Date().toISOString().substring(0, 16)
+    console.log(req.method + " " + req.url + " " + d)
+
+    // Handling request
+    if(static.staticResource(req)){
+        static.serveStaticResource(req, res)
+    }
+    else{
+        switch(req.method){
+            case "GET": 
+                // GET /alunos --------------------------------------------------------------------
+                if (req.url=='/alunos'){
+                    axios.get('http://localhost:3000/alunos')
+                        .then(function(resposta){
+                            res.writeHead(200, {'Content-Type': 'text/html'})
+                            res.end(templates.studentsListPage(resposta.data,d))
+                        })
+                        .catch(function(erro){
+                            res.writeHead(520, {'Content-Type': 'text/html'})
+                            res.end(templates.errorPage(erro,d))
+                        })
+                }
+                // GET /alunos/:id --------------------------------------------------------------------
+                else if (/\/alunos\/A[0-9]+/.test(req.url)){
+                    axios.get('http://localhost:3000'+req.url)
+                        .then(function(resposta){
+                            res.writeHead(200, {'Content-Type': 'text/html'})
+                            res.end(templates.studentPage(resposta.data,d))
+                        })
+                        .catch(function(erro){
+                            res.writeHead(520, {'Content-Type': 'text/html'})
+                            res.end(templates.errorPage(erro,d))
+                        })
+                }
+                // GET /alunos/registo --------------------------------------------------------------------
+                else if (req.url=='/alunos/registo'){
+                    res.writeHead(200, {'Content-Type': 'text/html'})
+                    res.end(templates.studentFormPage(d))
+                }
+                // GET /alunos/edit/:id --------------------------------------------------------------------
+                else if (/\/alunos\/edit\/A[0-9]+/.test(req.url)){
+                    var partes = req.url.split('/')
+                    idAluno = partes[partes.length-1]
+                    axios.get('http://localhost:3000/alunos/'+idAluno)
+                        .then(function(resposta){
+                            res.writeHead(200, {'Content-Type': 'text/html'})
+                            res.end(templates.studentFormEditPage(resposta.data,d))
+                        })
+                        .catch(function(erro){
+                            res.writeHead(525, {'Content-Type': 'text/html'})
+                            res.end(templates.errorPage(erro,d))
+                        })
+                }
+                // GET /alunos/delete/:id --------------------------------------------------------------------
+                else if(/\/alunos\/delete\/A[0-9]+/.test(req.url)){
+                    var partes = req.url.split('/')
+                    idAluno = partes[partes.length-1]
+                    axios.delete('http://localhost:3000/alunos/'+idAluno)
+                        .then(function(resposta){
+                            res.writeHead(200, {'Content-Type': 'text/html'})
+                            res.end(templates.studentPage(resposta.data,d))
+                        })
+                        .catch(function(erro){
+                            res.writeHead(530, {'Content-Type': 'text/html'})
+                            res.end(templates.errorPage(erro,d))
+                        })
+                }
+                // GET ? -> Lancar um erro
+                else{
+                    res.writeHead(404, {'Content-Type': 'text/html'})
+                    res.end(templates.errorPage(`Pedido GET nao suportado ${req.url}`,d))
+                }
+                break
+            case "POST":
+                // POST /alunos/registo --------------------------------------------------------------------
+                if (req.url== '/alunos/registo'){
+                    collectRequestBodyData(req, function(result){
+                        if(result){
+                            axios.post("http://localhost:3000/alunos",result)
+                                .then(function(resposta){
+                                    res.writeHead(201, {'Content-Type': 'text/html'})
+                                    res.end(templates.studentPage(resposta.data,d))
+                                })
+                                .catch(function(erro){
+                                    res.writeHead(520, {'Content-Type': 'text/html'})
+                                    res.end(templates.errorPage(erro,d))
+                                })
+                        }else{
+                            res.writeHead(404, {'Content-Type': 'text/html'})
+                            res.end(templates.errorPage(`Pedido POST nao foi possivel ${req.url}`,d))
+                        }
+                    })
+                }
+                // POST /alunos/edit/:id --------------------------------------------------------------------
+                else if (/\/alunos\/edit\/A[0-9]+/.test(req.url)){
+                    var partes = req.url.split('/')
+                    idAluno = partes[partes.length-1]
+                    console.log(idAluno)
+                    collectRequestBodyData(req, function(result){
+                        if(result){
+                            axios.put("http://localhost:3000/alunos/"+idAluno,result)
+                                .then(function(resposta){
+                                    res.writeHead(201, {'Content-Type': 'text/html'})
+                                    res.end(templates.studentPage(resposta.data,d))
+                                })
+                                .catch(function(erro){
+                                    res.writeHead(520, {'Content-Type': 'text/html'})
+                                    res.end(templates.errorPage(erro,d))
+                                })
+                        }else{
+                            res.writeHead(404, {'Content-Type': 'text/html'})
+                            res.end(templates.errorPage(`Pedido POST nao foi possivel ${req.url}`,d))
+                        }
+                    })
+                }
+                // POST ? -> Lancar um erro
+                else{
+                    res.writeHead(404, {'Content-Type': 'text/html'})
+                    res.end(templates.errorPage(`Pedido POST nao suportado ${req.url}`,d))
+                }
+            default: 
+                // Outros metodos nao sao suportados
+        }
+    }
+})
+
+alunosServer.listen(7777, ()=>{
+    console.log("Servidor à escuta na porta 7777...")
+})
---
+++ TP4/static.js
@@ -0,0 +1,33 @@
+var fs = require('fs')
+
+exports.staticResource = function(request){
+    return /\/w3.css$/.test(request.url) || 
+            /\/favicon.jpg$/.test(request.url)
+}
+
+exports.serveStaticResource = function(req, res){
+    var partes = req.url.split('/')
+    var file = partes[partes.length -1]
+    fs.readFile('public/' + file, (erro, dados)=>{
+        if(erro){
+            console.log('Erro: ficheiro não encontrado ' + erro)
+            res.statusCode = 404
+            res.end('Erro: ficheiro não encontrado ' + erro)
+        }
+        else{
+            if(file == 'favicon.ico'){
+                res.setHeader('Content-Type', 'image/x-icon')
+                res.end(dados)
+            }
+            else if(file == 'w3.css'){
+                res.setHeader('Content-Type', 'text/css')
+                res.end(dados)
+            }
+            // PNG images
+            else{
+                res.setHeader('Content-Type', 'image/png')
+                res.end(dados)
+            }    
+        }
+    })
+}
