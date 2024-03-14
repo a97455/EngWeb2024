bf506c0ab275d77963aa81b4e860857e1af3c662
a97455 <a97455@alunos.uminho.pt>
2024-03-14 11:43:14 +0000

    index

--- TP5/routes/index.js
+++ TP5/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios')
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  var d = new Date().toISOString().substring(0, 16)
+  res.render('index', { titulo: 'Gestão de Alunos', data: d});
 });
 
 module.exports = router;
