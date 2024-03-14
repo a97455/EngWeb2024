e2b4f9e3690eb0a25b0511366818ad17d5af51ef
a97455 <a97455@alunos.uminho.pt>
2024-03-14 12:47:02 +0000

    lista compositores

--- TP5/routes/index.js
+++ TP5/routes/index.js
@@ -5,7 +5,24 @@ var axios = require('axios')
 /* GET home page. */
 router.get('/', function(req, res) {
   var d = new Date().toISOString().substring(0, 16)
-  res.render('index', { titulo: 'Gestão de Alunos', data: d});
+  res.render('index', { titulo: 'Gestão de Compositores', data: d});
+});
+
+router.get('/compositores', function(req, res) {
+  var d = new Date().toISOString().substring(0, 16)
+  axios.get('http://localhost:3000/compositores')
+  .then(function(compositores){
+      axios.get('http://localhost:3000/periodos')
+      .then(function(periodos){
+        res.render('listaCompositores', {compositores: compositores.data, periodos: periodos.data, data: d, titulo: "Lista de Compositores"});
+      })
+      .catch(function(erro){
+        res.render('error',{error: erro, message: 'Erro ao recuperar os periodos'})
+      })
+  })
+  .catch(function(erro){
+      res.render('error',{error: erro, message: 'Erro ao recuperar os compositores'})
+  })
 });
 
 module.exports = router;
