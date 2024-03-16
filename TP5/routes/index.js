7c273c82fcbde79a593088ca0a8597cebc8bd3df
a97455 <a97455@alunos.uminho.pt>
2024-03-16 13:00:47 +0000

    tp5 finalizado

--- TP5/routes/index.js
+++ TP5/routes/index.js
@@ -8,6 +8,8 @@ router.get('/', function(req, res) {
   res.render('index', { titulo: 'Gestão de Compositores', data: d});
 });
 
+
+// /compositores
 router.get('/compositores', function(req, res) {
   var d = new Date().toISOString().substring(0, 16)
   axios.get('http://localhost:3000/compositores')
@@ -25,6 +27,8 @@ router.get('/compositores', function(req, res) {
   })
 });
 
+
+// /compositores/registo
 router.get('/compositores/registo', function(req, res) {
   axios.get('http://localhost:3000/periodos')
   .then(function(periodos){
@@ -54,6 +58,8 @@ router.post('/compositores/registo', function(req,res){
     })
 });
 
+
+// /compositores/:id
 router.get('/compositores/:id', function(req, res) { //no fim, pois apanha tudo /compositores  
   var d = new Date().toISOString().substring(0, 16)
   axios.get('http://localhost:3000/compositores/'+ req.params.id)
@@ -72,4 +78,54 @@ router.get('/compositores/:id', function(req, res) { //no fim, pois apanha tudo 
   })
 });
 
+
+// /compositores/edit/:id
+router.get('/compositores/edit/:id', function(req, res) {
+  axios.get('http://localhost:3000/periodos')
+  .then(function(periodos){
+    var d = new Date().toISOString().substring(0, 16)
+    res.render('editarCompositor', {periodos: periodos.data, data: d, titulo: "Edição de um Compositor"})  
+  })
+  .catch(function(erro){
+    res.render('error',{error: erro, message: 'Erro ao Recuperar os Compositores'})
+  })
+});
+
+router.post('/compositores/edit/:id', function(req,res){
+  var d = new Date().toISOString().substring(0, 16)
+  axios.put('http://localhost:3000/compositores/'+req.params.id,req.body)
+    .then(function(resposta){
+      var compositor = resposta.data
+      axios.get('http://localhost:3000/periodos/'+compositor.periodo)
+      .then(function(periodo){
+        res.render('compositor', {compositor: compositor, periodo: periodo.data, data: d, titulo: "Página do Compositor"});
+      })
+      .catch(function(erro){
+        res.render('error',{error: erro, message: 'Erro ao recuperar o periodo'})
+      })
+    })
+    .catch(function(erro){
+      res.render('error',{error: erro, message: 'Erro ao gravar um Aluno novo'})
+    })
+});
+
+
+// /compositores/delete/:id
+router.get('/compositores/delete/:id', function(req,res){
+  var d = new Date().toISOString().substring(0, 16)
+  axios.delete('http://localhost:3000/compositores/'+req.params.id)
+    .then(function(resposta){
+      var compositor = resposta.data
+      axios.get('http://localhost:3000/periodos/'+compositor.periodo)
+      .then(function(periodo){
+        res.render('compositor', {compositor: compositor, periodo: periodo.data, data: d, titulo: "Página do Compositor"});
+      })
+      .catch(function(erro){
+        res.render('error',{error: erro, message: 'Erro ao recuperar o periodo'})
+      })
+    })
+    .catch(function(erro){
+      res.render('error',{error: erro, message: 'Erro ao gravar um Aluno novo'})
+    })
+});
 module.exports = router;
