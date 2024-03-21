3360976ca7f5c8c0e4296ecd646bd9c3e1a404d5
a97455 <a97455@alunos.uminho.pt>
2024-03-21 11:14:39 +0000

    tp6

---
+++ TP6/README.md
@@ -0,0 +1,16 @@
+# TPC6: Aplicação Web Express c/ mongoDB
+## 2024-03-21
+
+## Autor
+
+- A97455
+- Henrique Nuno Marinho Malheiro
+
+## Resumo
+
+Criar uma aplicação para a gestão de uma base de dados de compositores musicais:
+
+- Montar a API de dados com o mongoDB a partir do dataset de compositores em anexo;
+
+- Criar uma aplicação Web com as seguintes caraterísticas, usando Express:
+    1. CRUD sobre compositores;
--- TP6/routes/compositores.js
+++ TP6/routes/compositores.js
@@ -1,9 +1,50 @@
 var express = require('express');
 var router = express.Router();
+var Compositor = require('../controllers/compositor')
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+
+router.get('/compositores', function(req, res) {
+  Compositor.list()
+  .then(function(data){
+    res.jsonp(data)
+  })
+  .catch(function(erro){
+    res.jsonp(erro)
+  })
 });
+
+
+router.get('/compositores/:id', function(req, res) {
+  Compositor.findById(req.params.id)
+  .then(function(data){
+    res.jsonp(data)
+  })
+  .catch(function(erro){
+    res.jsonp(erro)
+  })
+});
+
+
+router.post('/compositores', function(req, res) {
+  Compositor.insert(req.body)
+  .then(function(data){
+    res.jsonp(data)
+  })
+  .catch(function(erro){
+    res.jsonp(erro)
+  })
+});
+
+
+router.put('/compositores/:id', function(req, res) {
+  Compositor.update(req.params.id,req.body)
+  .then(function(data){
+    res.jsonp(data)
+  })
+  .catch(function(erro){
+    res.jsonp(erro)
+  })
+});
+
 
 module.exports = router;
